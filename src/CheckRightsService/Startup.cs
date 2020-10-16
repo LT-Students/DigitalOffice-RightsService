fcf30a9f4fc0eaa4bbc994eaf77df3a6a1075f93
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-10-16 20:31:02 +0300

    Add cors (#11)

--- src/CheckRightsService/Startup.cs
+++ src/CheckRightsService/Startup.cs
@@ -64,6 +64,14 @@ namespace LT.DigitalOffice.CheckRightsService
 
             app.UseRouting();
 
+            string corsUrl = Configuration.GetSection("Settings")["CorsUrl"];
+
+            app.UseCors(builder =>
+                builder
+                    .WithOrigins(corsUrl)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
