e951590c1203c96decfac9a0ea45c8762f8f6dc2
Lena <59228285+lalagvana@users.noreply.github.com>
2020-10-16 20:52:13 +0300

    Add/Edit/Remove departments Right (#9)
    * Added new right for Add/Edit/Remove departments
    
    * Update 20200802102340_addrightstodb.Designer.cs

--- src/CheckRightsService.Data.Provider.MsSql.Ef/Migrations/20200802102340_addrightstodb.cs
+++ src/CheckRightsService.Data.Provider.MsSql.Ef/Migrations/20200802102340_addrightstodb.cs
@@ -9,17 +9,22 @@ namespace LT.DigitalOffice.CheckRightsService.Data.Provider.MsSql.Ef.Migrations
             migrationBuilder.InsertData(
                 table: "Rights",
                 columns: new[] { "Id", "Description", "Name" },
-                values: new object[] { 1, null, "Add/Edit/Remove user" });
+                values: new object[] { 1, null, "Add/Edit/Remove users" });
 
             migrationBuilder.InsertData(
                 table: "Rights",
                 columns: new[] { "Id", "Description", "Name" },
-                values: new object[] { 2, null, "Add/Edit/Remove project" });
+                values: new object[] { 2, null, "Add/Edit/Remove projects" });
 
             migrationBuilder.InsertData(
                 table: "Rights",
                 columns: new[] { "Id", "Description", "Name" },
-                values: new object[] { 3, null, "Add/Edit/Remove email template" });
+                values: new object[] { 3, null, "Add/Edit/Remove email templates" });
+
+            migrationBuilder.InsertData(
+                table: "Rights",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 4, null, "Add/Edit/Remove departments" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -38,6 +43,11 @@ namespace LT.DigitalOffice.CheckRightsService.Data.Provider.MsSql.Ef.Migrations
                 table: "Rights",
                 keyColumn: "Id",
                 keyValue: 3);
+
+            migrationBuilder.DeleteData(
+               table: "Rights",
+               keyColumn: "Id",
+               keyValue: 4);
         }
     }
 }
