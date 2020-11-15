69225bc53ed0d9f040e69c056060bd0cdb540590
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-11-15 15:19:31 +0300

    Update GetUserInfoConsumer: add IsActive field

---
+++ src/CheckRightsService.Models.Broker/Responses/IGetUserResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LT.DigitalOffice.Broker.Responses
+{
+    public interface IGetUserResponse
+    {
+        Guid Id { get; }
+        string FirstName { get; }
+        string MiddleName { get; }
+        string LastName { get; set; }
+        bool IsActive { get; set; }
+
+        static object CreateObj(Guid id, string firstName, string middleName, string lastName, bool isActive)
+        {
+            return new
+            {
+                Id = id,
+                FirstName = firstName,
+                MiddleName = middleName,
+                LastName = lastName,
+                IsActive = isActive
+            };
+        }
+    }
+}
