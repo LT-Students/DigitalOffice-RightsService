82f02877f3878ca1cfffcb6a55a753d58c6b5681
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-07-15 18:32:38 +0300

    fix pagination (#34)
    * fix pagination
    
    * fix pagination
    
    * fix

--- src/RightsService.Data/RoleRepository.cs
+++ src/RightsService.Data/RoleRepository.cs
@@ -44,10 +44,20 @@ namespace LT.DigitalOffice.RightsService.Data
 
         public IEnumerable<DbRole> Find(int skipCount, int takeCount, out int totalCount)
         {
+            if (skipCount < 0)
+            {
+                throw new BadRequestException("Skip count can't be less than 0.");
+            }
+
+            if (takeCount <= 0)
+            {
+                throw new BadRequestException("Take count can't be equal or less than 0.");
+            }
+
             totalCount = _provider.Roles.Count();
 
             return _provider.Roles
-                .Skip(skipCount * takeCount)
+                .Skip(skipCount)
                 .Take(takeCount)
                 .Include(x => x.Rights)
                 .Include(x => x.Users);
