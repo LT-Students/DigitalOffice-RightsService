980c928febeb6de5c9165dc3de6496e9807eed57
Glunky <42372955+Glunky@users.noreply.github.com>
2021-08-11 23:04:21 +0300

    Small GetRoleCommand optimization + unit tests (#42)
    * Small GetRoleCommand optimization + unit tests
    
    * fixes of PR
    
    * add Verify

--- src/RightsService.Business/Commands/Role/GetRoleCommand.cs
+++ src/RightsService.Business/Commands/Role/GetRoleCommand.cs
@@ -4,6 +4,7 @@ using LT.DigitalOffice.Models.Broker.Responses.User;
 using LT.DigitalOffice.RightsService.Business.Role.Interfaces;
 using LT.DigitalOffice.RightsService.Data.Interfaces;
 using LT.DigitalOffice.RightsService.Mappers.Interfaces;
+using LT.DigitalOffice.RightsService.Models.Db;
 using LT.DigitalOffice.RightsService.Models.Dto.Models;
 using LT.DigitalOffice.RightsService.Models.Dto.Responses;
 using MassTransit;
@@ -23,18 +24,14 @@ namespace LT.DigitalOffice.RightsService.Business.Role
         private readonly IRoleInfoMapper _roleInfomapper;
         private readonly IUserInfoMapper _userInfoMapper;
         private readonly IRightResponseMapper _rightMapper;
-        private readonly IRequestClient<IGetUserDataRequest> _usersDataRequestClient;
+        private readonly IRequestClient<IGetUsersDataRequest> _usersDataRequestClient;
 
         private List<UserInfo> GetUsers(List<Guid> userIds, List<string> errors)
         {
-            string errorMessage = null;
-
             List<UserInfo> usersInfo = new();
 
             try
             {
-                errorMessage = $"Can not get users info for UserIds {string.Join('\n', userIds)}. Please try again later.";
-
                 var usersDataResponse = _usersDataRequestClient.GetResponse<IOperationResult<IGetUsersDataResponse>>(
                     IGetUsersDataRequest.CreateObj(userIds)).Result;
 
@@ -56,8 +53,7 @@ namespace LT.DigitalOffice.RightsService.Business.Role
             }
             catch (Exception exc)
             {
-                errors.Add(errorMessage);
-
+                errors.Add($"Can not get users info for UserIds {string.Join('\n', userIds)}. Please try again later.");
                 _logger.LogError(exc, "Exception on get user information.");
             }
 
@@ -71,7 +67,7 @@ namespace LT.DigitalOffice.RightsService.Business.Role
             IRoleInfoMapper roleInfoMapper,
             IUserInfoMapper userInfoMapper,
             IRightResponseMapper rightMapper,
-            IRequestClient<IGetUserDataRequest> usersDataRequestClient)
+            IRequestClient<IGetUsersDataRequest> usersDataRequestClient)
         {
             _logger = logger;
             _roleRepository = roleRepository;
@@ -86,17 +82,9 @@ namespace LT.DigitalOffice.RightsService.Business.Role
         {
             RoleResponse result = new();
 
-            var allRights = _rightRepository.GetRightsList();
-
-            var dbRole = _roleRepository.Get(roleId);
-
-            var rights = allRights
-                    .Where(x => dbRole.Rights.Select(x => x.RightId).Contains(x.Id))
-                    .Select(_rightMapper.Map)
-                    .ToList();
-
-            var users = GetUsers(dbRole.Users.Select(x => x.UserId).ToList(), result.Errors);
-
+            DbRole dbRole = _roleRepository.Get(roleId);
+            List<RightResponse> rights = dbRole.Rights.Select(r => r.Right).Select(_rightMapper.Map).ToList();
+            List<UserInfo> users = GetUsers(dbRole.Users.Select(x => x.UserId).ToList(), result.Errors);
             result.Role = _roleInfomapper.Map(dbRole, rights, users);
 
             return result;
