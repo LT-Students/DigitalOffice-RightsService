c2da526bc65a10251c14b8833ffc6f07bdd3fedc
ndoriomedov <doriomedov213@gmail.com>
2022-01-21 19:55:42 +0300

    add editRoleStatus validator +move cache methods from validators to helper

---
+++ src/RightsService.Validation/Helpers/Interfaces/ICheckRightsUniquenessHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Attributes;
+
+namespace LT.DigitalOffice.RightsService.Validation.Helpers.Interfaces
+{
+  [AutoInject]
+  public interface ICheckRightsUniquenessHelper
+  {
+    Task<bool> IsRightsSetUniqueAsync(IEnumerable<int> rightsIds);
+  }
+}
