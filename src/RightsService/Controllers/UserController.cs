1ff229703c8be86d80e469654824e5de83dbd13e
Dmitry Solovyov <dmitry.solovyov@lanit-tercom.com>
2022-04-28 14:14:14 +0300

    add controller

---
+++ src/RightsService/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.RightsService.Business.Commands.User.Interfaces;
+using LT.DigitalOffice.RightsService.Models.Dto.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.RightsService.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class UserController : ControllerBase
+  {
+    [HttpPut("edit")]
+    public async Task<OperationResultResponse<Guid?>> EditAsync(
+      [FromServices] IEditUserRoleCommand command,
+      [FromBody] EditUserRoleRequest request)
+    {
+      return await command.ExecuteAsync(request);
+    }
+  }
+}
