2a61d9bdb1d5202c9de9caf537d35d971460b3da
Nikita Doriomedov <88382379+ndoriomedov@users.noreply.github.com>
2022-09-13 13:40:21 +0300

    update redis methods (#82)

--- src/RightsService.Broker/Requests/UserService.cs
+++ src/RightsService.Broker/Requests/UserService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.BrokerSupport.Helpers;
+using LT.DigitalOffice.Kernel.RedisSupport.Constants;
+using LT.DigitalOffice.Kernel.RedisSupport.Extensions;
+using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
 using LT.DigitalOffice.Models.Broker.Common;
 using LT.DigitalOffice.Models.Broker.Models;
 using LT.DigitalOffice.Models.Broker.Requests.User;
@@ -16,15 +20,18 @@ namespace LT.DigitalOffice.RightsService.Broker.Requests
   {
     private readonly IRequestClient<ICheckUsersExistence> _rcCheckUserExistence;
     private readonly IRequestClient<IGetUsersDataRequest> _rcGetUsersDataRequest;
+    private readonly IGlobalCacheRepository _globalCache;
     private readonly ILogger<UserService> _logger;
 
     public UserService(
       IRequestClient<ICheckUsersExistence> rcCheckUserExistence,
       IRequestClient<IGetUsersDataRequest> rcGetUsersDataRequest,
+      IGlobalCacheRepository globalCache,
       ILogger<UserService> logger)
     {
       _rcCheckUserExistence = rcCheckUserExistence;
       _rcGetUsersDataRequest = rcGetUsersDataRequest;
+      _globalCache = globalCache;
       _logger = logger;
     }
 
@@ -40,12 +47,29 @@ namespace LT.DigitalOffice.RightsService.Broker.Requests
 
     public async Task<List<UserData>> GetUsersAsync(List<Guid> usersIds, List<string> errors)
     {
-      return (await RequestHandler.ProcessRequest<IGetUsersDataRequest, IGetUsersDataResponse>(
-          _rcGetUsersDataRequest,
-          IGetUsersDataRequest.CreateObj(usersIds),
+      if (usersIds is null || !usersIds.Any())
+      {
+        return null;
+      }
+
+      object request = IGetUsersDataRequest.CreateObj(usersIds);
+
+      List<UserData> usersData = await _globalCache.GetAsync<List<UserData>>(Cache.Users, usersIds.GetRedisCacheKey(request.GetBasicProperties()));
+
+      if (usersData is not null)
+      {
+        _logger.LogInformation(
+          "UsersDatas were taken from the cache. Users ids: {usersIds}", string.Join(", ", usersIds));
+      }
+      else
+      {
+        usersData = (await _rcGetUsersDataRequest.ProcessRequest<IGetUsersDataRequest, IGetUsersDataResponse>(
+          request,
           errors,
-          _logger))
-        ?.UsersData;
+          _logger))?.UsersData;
+      }
+
+      return usersData;
     }
   }
 }
