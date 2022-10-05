4d37fc631bd3ab43a5e5bac0d1e822549d9df6f2
Nikita Doriomedov <88382379+ndoriomedov@users.noreply.github.com>
2022-10-05 19:24:31 +0300

    remove cache from AccessValidatorConsumer (#87)
    * remove cache from AccessValidatorConsumer
    
    * update linq request
    
    * fix

--- src/RightsService.Broker/Consumers/AccessValidatorConsumer.cs
+++ src/RightsService.Broker/Consumers/AccessValidatorConsumer.cs
@@ -1,67 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using LT.DigitalOffice.Kernel.BrokerSupport.AccessValidatorEngine.Requests;
 using LT.DigitalOffice.Kernel.BrokerSupport.Broker;
-using LT.DigitalOffice.RightsService.Data.Interfaces;
-using LT.DigitalOffice.RightsService.Models.Db;
-using LT.DigitalOffice.RightsService.Models.Dto.Constants;
+using LT.DigitalOffice.RightsService.Data.Provider;
 using MassTransit;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
+using Microsoft.EntityFrameworkCore;
 
 namespace LT.DigitalOffice.RightsService.Broker.Consumers
 {
   public class AccessValidatorConsumer : IConsumer<ICheckUserRightsRequest>
   {
-    private readonly IUserRoleRepository _repository;
-    private readonly IRoleRepository _roleRepository;
-    private readonly IMemoryCache _cache;
+    private readonly IDataProvider _provider;
 
     private async Task<object> HasRightAsync(ICheckUserRightsRequest request)
     {
-      var users = _cache.Get<List<(Guid userId, Guid roleId)>>(CacheKeys.Users);
-      var rolesRights = _cache.Get<List<(Guid roleId, bool isActive, IEnumerable<int> rights)>>(CacheKeys.RolesRights);
-
-      if (users == null)
-      {
-        List<DbUserRole> dbUsers = await _repository.GetWithRightsAsync();
-        users = dbUsers.Select(x => (x.UserId, x.RoleId)).ToList();
-        _cache.Set(CacheKeys.Users, users);
-      }
-      
-      if (rolesRights is null)
-      {
-        List<DbRole> dbRoles = await _roleRepository.GetAllWithRightsAsync();
-        rolesRights = dbRoles.Select(x => (x.Id, x.IsActive, x.RolesRights.Select(x => x.RightId))).ToList();
-        _cache.Set(CacheKeys.RolesRights, rolesRights);
-      }
-
-      (Guid userId, Guid roleId) user = users.FirstOrDefault(x => x.userId == request.UserId);
-      (Guid roleId, bool isActive, IEnumerable<int> rights) role = rolesRights.FirstOrDefault(x => x.roleId == user.roleId && x.isActive);
-
-      foreach (int rightId in request.RightIds)
-      {
-        if (role.rights?.Contains(rightId) == true)
-        {
-          continue;
-        }
-
-        return false;
-      }
-
-      return true;
+      return request.RightIds.Intersect(await
+          (from user in _provider.UsersRoles
+           where user.UserId == request.UserId && user.IsActive
+           join role in _provider.Roles on user.RoleId equals role.Id
+           where role.IsActive
+           join rolesRights in _provider.RolesRights on role.Id equals rolesRights.RoleId
+           select rolesRights.RightId)
+          .ToListAsync())
+        .Count() == request.RightIds.Count();
     }
 
-    public AccessValidatorConsumer(
-      [FromServices] IUserRoleRepository repository,
-      [FromServices] IRoleRepository roleRepository,
-      [FromServices] IMemoryCache cache)
+    public AccessValidatorConsumer(IDataProvider provider)
     {
-      _repository = repository;
-      _roleRepository = roleRepository;
-      _cache = cache;
+      _provider = provider;
     }
 
     public async Task Consume(ConsumeContext<ICheckUserRightsRequest> context)
